da0f37bd7815029e3100be500cbe6f5b5688ea68
DaviMateusSilveira <david2fagulha@gmail.com>
2025-09-22 19:26:53 -0300

    initial commit

---
+++ gerador-curriculo/src/main/java/com/engenharia_softeare/gerador_curriculo/GeradorCurriculoApplication.java
@@ -0,0 +1,13 @@
+package com.engenharia_softeare.gerador_curriculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorCurriculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorCurriculoApplication.class, args);
+	}
+
+}
---
+++ gerador-curriculo/src/test/java/com/engenharia_softeare/gerador_curriculo/GeradorCurriculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engenharia_softeare.gerador_curriculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorCurriculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
